d79a5c3800daf79be86428efdf778bf08f7563fa
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2025-07-11 23:27:34 -0400

    i don't know anymore

--- bot.py
+++ bot.py
@@ -624,8 +624,7 @@ async def list_commands(
     Usage: /list_commands [page_number|command_name] [ephemeral]
     If ephemeral is true, only you can see the command output (other users cannot see or interact with it).
     """
-    print(f"Interaction user: {interaction.user.name}")
-    await interaction.response.defer(ephemeral=ephemeral)
+    await interaction.response.defer(ephemeral=False)
     update_command_list()
 
     COMMANDS_PER_PAGE = 10
