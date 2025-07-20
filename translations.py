f01f39cb316efe1490e116b5793859b245fca8bd
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2025-07-19 23:45:38 -0400

    for some reason my env is throwing a fit about return types

--- translations.py
+++ translations.py
@@ -5,7 +5,7 @@ class BotTranslator(app_commands.Translator):
     
     async def translate(
         self, 
-        string: app_commands.locale_str, locale: Locale, context: app_commands.TranslationContext ) -> str | None:
+        string: app_commands.locale_str, locale: Locale, context: app_commands.TranslationContext ):
         
         if context.name == "make-command":  # check command name
             if locale is Locale.japanese:  # check locale
