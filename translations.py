651529da76f7f81bd320dd607790223749e6ad50
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2025-07-19 23:47:54 -0400

    old docs are stupid, at least mention the version

--- translations.py
+++ translations.py
@@ -6,8 +6,8 @@ class BotTranslator(app_commands.Translator):
     async def translate(
         self, 
         string: app_commands.locale_str, locale: Locale, context: app_commands.TranslationContext ):
-        
-        if context.name == "make-command":  # check command name
+        print(string, locale, context)  # Debugging output to check the parameters
+        if context.location == "make-command":  # check command name
             if locale is Locale.japanese:  # check locale
                 print("translated")
                 return "コマンドを作成する"  # return translated string
