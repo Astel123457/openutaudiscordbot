2df7814f5bedbc4ea7f9f1e1017254247584d5cf
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2025-07-22 01:38:09 -0400

    this is prolly horribly broken

--- translations.py
+++ translations.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import discord
 from discord import app_commands
+# this apperenlty doesn't work but we'll leave it for now, just in case things start working again
 
 class MyTranslator(app_commands.Translator):
     async def translate(
