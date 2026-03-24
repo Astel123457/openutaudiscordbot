87d806bdce7bdadb44af202b1535c096c6c12492
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2026-03-23 23:10:33 -0400

    undo that bc people don't need to know

--- bot.py
+++ bot.py
@@ -87,12 +87,7 @@ async def on_ready():
     print(f"Logged in as {client.user.name}")
     print(f"Bot ID: {client.user.id}")
     print("Client has started")
-
-@client.command()
-async def listservers(ctx):
-    server_list = "\n".join([f"{g.name} ({g.id})" for g in client.guilds])
-    await ctx.send(f"I am in these servers:\n{server_list}")
-
+    
 @client.event
 async def on_message(message: discord.Message):
     global stop_flag
